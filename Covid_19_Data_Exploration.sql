f2824592f9930fba9a240143fa3ba9e12b284a2e
Pham Dinh Tuan <136052582+Mahptuan@users.noreply.github.com>
2023-06-25 22:33:50 +0700

    Update Covid_19_Data_Exploration.sql

--- Covid_19_Data_Exploration.sql
+++ Covid_19_Data_Exploration.sql
@@ -1,16 +1,11 @@
-/*
-Covid 19 Data Exploration 
-
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
-
-*/
+#Covid 19 Data Exploration 
 
 Select *
 FROM PortfolioProject..CovidDeaths
 Where continent is not null 
 order by 3,4
 
-
+	
 -- Select Data that we are going to be starting with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
